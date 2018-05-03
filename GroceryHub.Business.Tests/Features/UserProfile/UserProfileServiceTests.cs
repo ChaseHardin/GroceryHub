19e60a56c966c64c4eddf8666cb2546daa382dc5
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-05-02 21:37:20 -0500

    Add business level testing project

---
+++ GroceryHub.Business.Tests/Features/UserProfile/UserProfileServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using GroceryHub.Business.Features.UserProfile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryHub.Business.Tests.Features.UserProfile
+{
+    [TestClass]
+    public class UserProfileServiceTests
+    {
+        [TestMethod]
+        public void GetProfile_returns_chase_and_makenna()
+        {
+            var service = new UserProfileService();
+            var actual = service.GetProfile();
+
+            var chase = actual.First(x => x.FirstName.ToLower() == "chase");
+            var makenna = actual.First(x => x.FirstName.ToLower() == "makenna");
+
+            Assert.AreEqual(actual.Count(), 2);
+            Assert.AreEqual(chase.FirstName, "Chase");
+            Assert.AreEqual(chase.LastName, "Hardin");
+            Assert.AreEqual(makenna.FirstName, "Makenna");
+            Assert.AreEqual(makenna.LastName, "Ridgway");
+        }
+    }
+}
